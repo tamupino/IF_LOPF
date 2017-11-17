8349af961226f0aa6a9626a26589d509cce2d74d
lorozcoHKS <33663939+lorozcoHKS@users.noreply.github.com>
2017-11-17 12:07:32 +0000

    171116 - Commit 02

--- 01_Field.py
+++ 01_Field.py
@@ -41,12 +41,11 @@ def corners(field_boundary_lines, corner_radius):
 
         # FieldEdgeEnd = field_edge.PointAtEnd()
         # NextFieldEdgeStart = next_field_edge.PointAtStart()
-        FieldBoundaryFilletSet = rg.Curve.CreateFilletCurves(field_edge.ToNurbsCurve(), field_edge.To, next_field_edge.ToNurbsCurve(),
-                                                             next_field_edge.To, corner_radius, False, True, False,
-                                                             rd.ActiveDoc.ModelAbsoluteTolerance,
-                                                             rd.ActiveDoc.ModelAbsoluteTolerance)
-        # FieldFilletNum =
-        field_boundary_fillet = FieldBoundaryFilletSet[-1]
+        field_boundary_fillet_set = rg.Curve.CreateFilletCurves(field_edge.ToNurbsCurve(), field_edge.To,
+                                    next_field_edge.ToNurbsCurve(), next_field_edge.To, corner_radius,
+                                    False, True, False, rd.ActiveDoc.ModelAbsoluteTolerance,
+                                    rd.ActiveDoc.ModelAbsoluteTolerance)
+        field_boundary_fillet = field_boundary_fillet_set[-1]
         field_boundary_corners.append(field_boundary_fillet)
         # b.append(FieldBoundaryCurves[0])
         # d.append(FieldBoundaryCurves[1])
@@ -69,28 +68,42 @@ def corners(field_boundary_lines, corner_radius):
 
 def boards(field_boundary_polyline, boards_distance, boards_height):
     field_boundary_curve = field_boundary_polyline.ToNurbsCurve()
-    field_boundary_curves = []
-    # for field_boundary_segment in field_boundary_polyline:
-    #     field_boundary_segment.ToNurbsCurve()
-    #     field_boundary_curves.append(field_boundary_segment)
     if boards_distance is not False:
         boards_bottom_curve = field_boundary_curve.Offset(rg.Plane.WorldXY, boards_distance,
                               rd.ActiveDoc.ModelAbsoluteTolerance, rg.CurveOffsetCornerStyle.Sharp)[0]
     else:
         boards_bottom_curve = field_boundary_polyline
-    # boards_bottom_curve = rg.Polyline.ToNurbsCurve(field_boundary_polyline)
     boards_vertical_vector = rg.Vector3d(0, 0, boards_height)
-    # boards_vertical_vector.Transform()
     boards_surface = rg.Surface.CreateExtrusion(boards_bottom_curve, boards_vertical_vector)
-    # boards_top_curve = boards_bottom_curve.Transform(boards_vertical_vector)
     xform = rg.Transform.Translation(boards_vertical_vector)
     boards_top_curve = boards_bottom_curve.Transform(xform)
     print type(boards_bottom_curve)
-    return boards_surface, boards_bottom_curve
+    return boards_surface, boards_top_curve
+
+
+def focalpoint(field_boundary_polyline, focalpoint_distance, focalpoint_height):
+    field_boundary_curve = field_boundary_polyline.ToNurbsCurve()
+    focalpoint_offset_curve = field_boundary_curve.Offset(rg.Plane.WorldXY, -focalpoint_distance,
+                       rd.ActiveDoc.ModelAbsoluteTolerance, rg.CurveOffsetCornerStyle.Sharp)[0]
+    if focalpoint_height is not False:
+        focalpoint_vertical_vector = rg.Vector3d(0,0, focalpoint_height)
+        xform = rg.Transform.Translation(focalpoint_vertical_vector)
+        focalpoint_curve = focalpoint_offset_curve.Transform(xform)
+    else:
+        focalpoint_curve = focalpoint_offset_curve
+    return focalpoint_curve
+
 
 def safetyzone(field_boundary_segments, safetyzone_end_dist, safetyzone_lat_dist):
-    field_boundary_curves = field_boundary_segments.ToNurbsCurve()
-    field_boundary_polyline = rg.Curve.JoinCurves(field_boundary_segments)
+    centre_of_field = rg.Point(WorldOrigin)
+    for i in len(field_boundary_segments):
+        field_boundary_segment = field_boundary_segments[i]
+        field_boundary_segment_mid = rg.Point.Midpoint(field_boundary_segment)
+        safetyzone_vector = rg.Vector3d(centre_of_field, field_boundary_segment_mid)
+        xform = rg.Transform.Translation(safetyzone_vector)
+        safetyzone_segment = field_boundary_segment.Transform(xform)
+        safetyzone_segments.append(safetyzone_segment)
+    return safetyzone_segments
 
     # for i in range(len(field_boundary_segments)):
 
@@ -101,25 +114,80 @@ def minimumsweep(safety_offset, boundary_curves):
 
 FieldLength = []
 FieldWidth = []
+FieldCornerRadius = []
+BoardsDistance = []
+BoardsHeight = []
+FocalPointDistance = []
+FocalPointHeight = []
+SafetyZoneEnds = []
+SafetyZoneLats = []
 
-if SportType == 0
+if SportType == 0:
+    # Hockey
     FieldLength = 60
+    FieldWidth = 30
+    FieldCornerRadius = 8.5
+    BoardsDistance = 0
+    BoardsHeight = 1.2
+    FocalPointDistance = 4
+    FocalPointHeight = 0
+    SafetyZoneEnds = 0
+    SafetyZoneLats = 0
+elif SportType == 1:
+    # FIFA Football
+    FieldLength = 105
+    FieldWidth = 68
+    FieldCornerRadius = 0
+    BoardsDistance = 5
+    BoardsHeight = 1
+    FocalPointDistance = 0
+    FocalPointHeight = 0
+    SafetyZoneEnds = 10
+    SafetyZoneLats = 8.5
+elif SportType == 2:
+    # Rugby League
+    FieldLength = []
+    FieldWidth = []
+    FieldCornerRadius = []
+    BoardsDistance = []
+    BoardsHeight = []
+    FocalPointDistance = []
+    FocalPointHeight = []
+    SafetyZoneDistance = []
+elif SportType == 3:
+    # Rugby Union
+    FieldLength = []
+    FieldWidth = []
+    FieldCornerRadius = []
+    BoardsDistance = []
+    BoardsHeight = []
+    FocalPointDistance = []
+    FocalPointHeight = []
+    SafetyZoneDistance = []
     # also creates a series of other options for the secondary selection
 
 # The Part of the Code that Runs the Functions
-
+# Create Field Boundary
 FieldOutput = field(FieldLength, FieldWidth)
 FieldBoundaryPolyline = FieldOutput[0]
 FieldBoundarySegments = FieldOutput[1]
+# if rounded corners, create rounded field corners
 if FieldCornerRadius is not False:
     Output = corners(FieldBoundarySegments, FieldCornerRadius)
-    BoundaryCorners = Output[0]
-    BoundarySides = Output[1]
+    BoundarySegments = Output[0]
+    BoundaryJoinedCurve = Output[1]
+    FieldBoundaryPolyline = BoundaryJoinedCurve
+    FieldBoundarySegments = BoundarySegments
+# if Offset Focal Point, create offset focal point, else take Field Boundary as focal point
+if FocalPointDistance is not False:
+    FocalPointOutput = focalpoint(FieldBoundaryPolyline, FocalPointDistance, FocalPointHeight)
+    FocalPointCurve = FocalPointOutput[0]
+# if Boards, Create Boards
 if BoardsDistance is not False:
     BoardsOutput = boards(FieldBoundaryPolyline, BoardsDistance, BoardsHeight)
-    # BoardsSurface = BoardsOutput[0]
-    # BoardsTopCurve = BoardsOutput[1]
-#         weave(FieldBoundaryCorners)
+    BoardsSurface = BoardsOutput[0]
+    BoardsTopCurve = BoardsOutput[1]
+
 # Offset Focal Point
 # if(FocalOffsetDistance != False):
 #    FocalOffset = rg.Curve.Offset(rg.Plane.WorldXY, FocalOffsetDistance, )
